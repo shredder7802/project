7c38b7ae7a949d1dbfd1b23a1d6547b8968b0e37
shredder7802 <nikitastrim738@gmail.com>
2026-03-24 16:37:41 +0700

    year project

---
+++ base.py
@@ -0,0 +1,36 @@
+import sqlite3
+
+class SQL:
+    def __init__(self, database):
+        self.connection = sqlite3.connect(database)
+        self.cursor = self.connection.cursor()
+
+    # Добавление пользователя в БД
+    def add_user(self, id):
+        query = "INSERT INTO users (id) VALUES(?)"
+        with self.connection:
+            return self.cursor.execute(query, (id,))
+
+    # Проверка, есть ли пользователь в БД
+    def user_exist(self, id):
+        query = "SELECT * FROM users WHERE id = ?"
+        with self.connection:
+            result = self.cursor.execute(query, (id,)).fetchall()
+            return bool(len(result))
+
+    # Получить значение поля
+    def get_field(self, table, id, field):
+        query = f"SELECT {field} FROM {table} WHERE id = ?"
+        with self.connection:
+            result = self.cursor.execute(query, (id,)).fetchone()
+            if result:
+                return result[0]
+
+    # Обновить значение поля
+    def update_field(self, table, id, field, value):
+        query = f"UPDATE {table} SET {field} = ? WHERE id = ?"
+        with self.connection:
+            self.cursor.execute(query, (value, id))
+
+    def close(self):
+        self.connection.close()
---
+++ main.py
@@ -0,0 +1,119 @@
+import config
+import logging
+import asyncio
+from aiogram import Bot, Dispatcher, F
+from base import SQL  # подключение класса SQL из файла base
+from aiogram.types import ReplyKeyboardRemove, \
+    ReplyKeyboardMarkup, KeyboardButton, \
+    InlineKeyboardMarkup, InlineKeyboardButton
+from datetime import datetime
+
+
+db = SQL('db.db')  # соединение с БД
+
+bot = Bot(token=config.TOKEN)
+dp = Dispatcher()
+
+logging.basicConfig(level=logging.INFO)
+
+#inline клавиатура
+kb_menu = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text="Добавить событие",callback_data="new_event")],
+    [InlineKeyboardButton(text="Мои события", callback_data="my_events")],
+    [InlineKeyboardButton(text="Помощь", callback_data="help")],
+    [InlineKeyboardButton(text="Настройки", callback_data="settings")]
+])
+
+kb_ = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text="Добавить событие",callback_data="new_event")],
+    [InlineKeyboardButton(text="Мои события", callback_data="my_events")],
+    [InlineKeyboardButton(text="Помощь", callback_data="help")],
+    [InlineKeyboardButton(text="Настройки", callback_data="settings")]
+])
+
+Keyboard = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text="/start", callback_data="")]
+             ],
+        one_time_keyboard=True
+)
+
+buttons = [
+        [KeyboardButton(text="/start")]
+    ]
+kb = ReplyKeyboardMarkup(keyboard=buttons, resize_keyboard=True, one_time_keyboard=True)
+
+kb_events = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text="Удалить событие",callback_data="delete_event")],
+    [InlineKeyboardButton(text="Изменить время", callback_data="my_events")],
+    [InlineKeyboardButton(text="помощь", callback_data="help")],
+    [InlineKeyboardButton(text="Настройки", callback_data="settings")]
+])
+kb_comment = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text="Открыть комментарий",callback_data="open_comment")]
+    ])
+#когда пользователь написал/start
+
+
+
+#когда пользователь написал сообщение
+@dp.message()
+async def start(message):
+    global name, time, comment
+    id = message.from_user.id
+    if not db.user_exist(id):#если пользователя нет в бд
+        db.add_user(id)#добавляем
+    status = db.get_field("users", id, "status")  # получаем статус пользователя
+    if message.text == "/start":
+        db.update_field("users", id, "status", 1) #изменяем статус пользователя
+        await message.answer("Главное меню: ", reply_markup=kb_menu)#отправка сообщения с клавиатурой
+    if status == 2:
+        name = message.text
+        await message.answer('Введите комментарий к  событию, если он не нужен напишите: "-" ')
+        db.update_field("users", id, "status", 3)
+        return
+    if status == 3:
+        comment = message.text
+        await message.answer("Теперь укажи дату и время в формате ДД.ММ.ГГГГ ЧЧ:ММ")
+        #if len(time) != 16:
+           # await message.answer("Неправильно! Укажи дату и время в формате ДД.ММ.ГГГГ ЧЧ:ММ")
+        db.update_field("users", id, "status", 4)
+        return
+    if status == 4:
+        time = message.text
+        db.get_field("events", id, "name")
+        db.get_field("events", id, "time")
+        db.get_field("events", id, "comment")
+        await message.answer(f"Событие: {name}, начнется: {time}", reply_markup=kb_comment)
+        db.update_field("users", id, "status", 4)
+
+        return
+#когда пользователь нажал на inline кнопку
+@dp.callback_query()
+async def start_call(call):
+    id = call.from_user.id
+    if not db.user_exist(id):#если пользователя нет в бд
+        db.add_user(id)#добавляем
+    if call.data == "new_event":  # проверка нажатия на кнопку
+        db.update_field("users", id, "status", 2)
+        await call.message.answer("Введите название события: ")
+    if call.data == "open_comment":  # проверка нажатия на кнопку
+        await call.message.answer(f"Ваш комментарий: {comment}")
+
+
+
+
+
+
+
+    #await call.answer("Оповещение сверху")
+    #await call.message.answer("Отправка сообщения")
+    #await call.message.edit_text("Редактирование сообщения")
+    #await call.message.delete()#удаление сообщения
+    await bot.answer_callback_query(call.id)#ответ на запрос, чтобы бот не зависал
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
